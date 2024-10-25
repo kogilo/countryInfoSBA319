59e64f537cbb72042d1f3b5e9e0e402a949175a5
kogilo <getabullaothow@gmail.com>
2024-10-24 22:21:14 -0500

    all routes added

--- routes/countryRoutes.mjs
+++ routes/countryRoutes.mjs
@@ -18,4 +18,43 @@ router.post('/api', async (req, res) => {
   });
 
 
+  // Read
+router.get('/', async (req, res) => {
+  try {
+    const allCountries = await Country.find({});
+
+    res.json(allCountries);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+});
+
+
+// Update
+router.put('/:id', async (req, res) => {
+  try {
+    let updatedCountry = await Country.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.json(updatedCountry);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+});
+// Delete
+router.delete('/:id', async (req, res) => {
+  try {
+    let deletedCountry = await Country.findByIdAndDelete(req.params.id);
+
+    res.json(deletedCountry);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+});
+
+
   export default router;
